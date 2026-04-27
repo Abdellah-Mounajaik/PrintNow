87ed1edc3a84f149ce565af9f4c25ec7c5329f5e
Abdellah Mounajaik <mounajaikabdellah@gmail.com>
2026-04-27 15:32:01 +0200

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/printnow/PrintnowApplication.java
@@ -0,0 +1,13 @@
+package com.printnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printnow/PrintnowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
